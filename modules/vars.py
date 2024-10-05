e10851e6be9fdfd737b9abfe95b7608db9168601
AARABHSINGH <168511707+AARABHSINGH@users.noreply.github.com>
2024-10-05 23:39:10 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,6 +2,6 @@ import os
 
 API_ID    = os.environ.get("API_ID", "21546895")
 API_HASH  = os.environ.get("API_HASH", "3fbb1254728cf2ab3447a0e9e7aa6fc0")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7537819246:AAHjEXDODtoE1KIrxIwVBNbZO_gfByWDlrE") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7711122186:AAF3aNiDLFZlsn4aM47-U01ah1a2YlVkDIc") 
 WEBHOOK = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
