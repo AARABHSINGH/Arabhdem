f201cebf8d1e1010b91178379b869318df2100ac
AARABHSINGH <168511707+AARABHSINGH@users.noreply.github.com>
2024-10-31 13:18:10 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -26,7 +26,7 @@ bot = Client(
     "bot",
     api_id=28466214,
     api_hash= "3f55d44aae0f6c72f0dd8855adeeb60f",
-    bot_token= "8147258257:AAEF6zxcPVVcEw_TV7IAhnw8NIcbSkLsOHA")
+    bot_token= "7517893635:AAEto1Z62lr50YemTSMZKFUNB2OKwKQK-ZI")
 
 
 @bot.on_message(filters.command(["start"]))
